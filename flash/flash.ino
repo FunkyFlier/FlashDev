383b6a15a8b41435cd4450a90d0dbf1920596e56
FunkyFlier <mike@kaslingaircraft.com>
2015-10-30 09:38:40 -0500

    Initial commit

---
+++ flash/flash.ino
@@ -0,0 +1,332 @@
+#include <SPI.h>
+#include <Streaming.h>
+
+//however digitalWrite will work when using SPI 
+#define GyroSSOutput() DDRL |= 1<<0 
+#define GyroSSHigh() PORTL |= 1<<0 
+#define GyroSSLow() PORTL &= ~(1<<0)
+
+#define AccSSOutput() DDRL |= 1<<1 
+#define AccSSHigh() PORTL |= 1<<1 
+#define AccSSLow() PORTL &= ~(1<<1)
+
+#define BaroSSOutput() DDRL |= 1<<2 
+#define BaroSSHigh() PORTL |= 1<<2 
+#define BaroSSLow() PORTL &= ~(1<<2)
+
+#define MagSSOutput() DDRL |= 1<<3 
+#define MagSSHigh() PORTL |= 1<<3
+#define MagSSLow() PORTL &= ~(1<<3)
+
+#define FlashSSOutput() DDRL |= 1<<4
+#define FlashSSHigh() PORTL |= 1<<4
+#define FlashSSLow() PORTL &= ~(1<<4)
+
+
+#define READ_ARRAY 0x03
+#define ERASE_4K 0x20
+#define ERASE_32K 0x52
+#define ERASE_64K 0xD8
+#define ERASE_CHIP 0xC7
+#define PROGRAM_PAGE 0x02
+
+#define WRITE_ENABLE 0x06
+
+#define READ_STATUS_REG 0x05
+
+#define STATUS_WRITE 0x01
+
+typedef union{
+  uint32_t val;
+  uint8_t buffer[4];
+}
+uint32_u;
+
+typedef union {
+  float val;
+  uint8_t buffer[4];
+}
+float_u;
+
+
+uint32_u pageIndex;
+
+uint8_t buffer[256];
+
+uint32_t time;
+
+#define ADDRESS_LIMIT 0x3FFFFF
+
+uint32_t addressIndex;
+uint8_t statusByte;
+float_u outFloat;
+
+void setup(){
+  Serial.begin(115200);
+
+  GyroSSOutput();
+  AccSSOutput();
+  BaroSSOutput();
+  MagSSOutput();
+  FlashSSOutput();
+  GyroSSHigh();
+  AccSSHigh();
+  BaroSSHigh();
+  MagSSHigh();
+  FlashSSHigh();
+
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);   
+  SPI.setDataMode(SPI_MODE0);
+
+  pageIndex.val = 0;
+
+  EraseChip();
+
+  delay(5);
+  FlashSSLow();
+  SPI.transfer(WRITE_ENABLE);
+  //SPI.transfer(0x00);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(STATUS_WRITE);
+  SPI.transfer(0x00);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(READ_STATUS_REG);
+  Serial.println(SPI.transfer(0),HEX);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(WRITE_ENABLE);
+  //SPI.transfer(0x00);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(READ_STATUS_REG);
+  Serial.println(SPI.transfer(0),HEX);
+  FlashSSHigh();
+  delay(5);
+
+  FlashSSLow();
+  SPI.transfer(READ_ARRAY);
+  SPI.transfer(pageIndex.buffer[2]);
+  SPI.transfer(pageIndex.buffer[1]);
+  SPI.transfer(pageIndex.buffer[0]);
+  for(int i = 0; i <= 255; i++){
+    buffer[i] = SPI.transfer(0);
+    //Serial.println(SPI.transfer(0),HEX);
+  }
+
+  FlashSSHigh();
+  Serial<<"++++\r\n";
+  for(int i = 0; i <= 255; i++){
+    Serial.println(buffer[i]);
+  }
+  /*
+  FlashSSLow();
+   SPI.transfer(ERASE_CHIP);
+   FlashSSHigh();
+   delay(1000);
+   
+   FlashSSLow();
+   SPI.transfer(READ_STATUS_REG);
+   Serial.println(SPI.transfer(0),HEX);
+   FlashSSHigh();*/
+
+  /*delay(5);
+   
+   
+   FlashSSLow();
+   SPI.transfer(STATUS_WRITE);
+   SPI.transfer(0x00);
+   FlashSSHigh();
+   
+   delay(5);
+   
+   FlashSSLow();
+   SPI.transfer(READ_STATUS_REG);
+   Serial.println(SPI.transfer(0),HEX);
+   FlashSSHigh();
+   
+   delay(5);
+   
+   FlashSSLow();
+   SPI.transfer(WRITE_ENABLE);
+   SPI.transfer(0x00);
+   FlashSSHigh();
+   
+   delay(5);
+   
+   FlashSSLow();
+   SPI.transfer(READ_STATUS_REG);
+   Serial.println(SPI.transfer(0),HEX);
+   FlashSSHigh();
+   */
+  delay(5);
+
+  //SPI.transfer(WRITE_ENABLE);
+
+  /*for(int i = 0; i <= 255; i++){
+   pageIndex.val = i;
+   FlashSSLow();
+   SPI.transfer(PROGRAM_PAGE);
+   SPI.transfer(pageIndex.buffer[2]);
+   SPI.transfer(pageIndex.buffer[1]);
+   SPI.transfer(pageIndex.buffer[0]);
+   SPI.transfer((uint8_t)i);
+   FlashSSHigh();
+   delay(5);
+   }
+   pageIndex.val = 0;*/
+
+  delay(5);
+  FlashSSLow();
+  SPI.transfer(WRITE_ENABLE);
+  //SPI.transfer(0x00);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(STATUS_WRITE);
+  SPI.transfer(0x00);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(READ_STATUS_REG);
+  Serial.println(SPI.transfer(0),HEX);
+  FlashSSHigh();
+
+  delay(5);
+  FlashSSLow();
+  SPI.transfer(WRITE_ENABLE);
+  //SPI.transfer(0x00);
+  FlashSSHigh();
+  
+  FlashSSLow();
+  SPI.transfer(READ_STATUS_REG);
+  Serial.println(SPI.transfer(0),HEX);
+  FlashSSHigh();
+
+  FlashSSLow();
+  //SPI.transfer(WRITE_ENABLE);
+  SPI.transfer(PROGRAM_PAGE);
+  SPI.transfer(pageIndex.buffer[2]);
+  SPI.transfer(pageIndex.buffer[1]);
+  SPI.transfer(pageIndex.buffer[0]);
+  for(int i = 0; i <= 255; i++){
+    SPI.transfer((uint8_t)i);
+  }
+  FlashSSHigh();
+
+  delay(5);
+
+  FlashSSLow();
+  SPI.transfer(READ_ARRAY);
+  SPI.transfer(pageIndex.buffer[2]);
+  SPI.transfer(pageIndex.buffer[1]);
+  SPI.transfer(pageIndex.buffer[0]);
+  for(int i = 0; i <= 255; i++){
+    buffer[i] = SPI.transfer(0);
+    //Serial.println(SPI.transfer(0),HEX);
+  }
+
+  FlashSSHigh();
+  Serial<<"****\r\n";
+  for(int i = 0; i <= 255; i++){
+    Serial.println(buffer[i]);
+  }
+
+  /*FlashSSLow();
+   
+   SPI.transfer(READ_STATUS_REG);
+   Serial.println(SPI.transfer(0),HEX);
+   
+   FlashSSHigh();
+   
+   FlashSSLow();
+   SPI.transfer(STATUS_WRITE);
+   SPI.transfer(0);
+   SPI.transfer(WRITE_ENABLE);
+   SPI.transfer(READ_STATUS_REG);
+   Serial.println(SPI.transfer(0),HEX);
+   
+   FlashSSHigh();
+   
+   FlashSSLow();
+   SPI.transfer(WRITE_ENABLE);
+   FlashSSHigh();
+   
+   delay(100);
+   
+   FlashSSLow();
+   SPI.transfer(WRITE_ENABLE);
+   SPI.transfer(PROGRAM_PAGE);
+   SPI.transfer(pageIndex.buffer[2]);
+   SPI.transfer(pageIndex.buffer[1]);
+   SPI.transfer(pageIndex.buffer[0]);
+   for(int i = 0; i <= 255; i++){
+   SPI.transfer((uint8_t)i);
+   }
+   FlashSSHigh();
+   
+   delay(100);
+   
+   FlashSSLow();
+   SPI.transfer(READ_ARRAY);
+   SPI.transfer(pageIndex.buffer[2]);
+   SPI.transfer(pageIndex.buffer[1]);
+   SPI.transfer(pageIndex.buffer[0]);
+   for(int i = 0; i <= 255; i++){
+   buffer[i] = SPI.transfer(0);
+   //Serial.println(SPI.transfer(0),HEX);
+   }
+   
+   FlashSSHigh();
+   
+   for(int i = 0; i <= 255; i++){
+   Serial.println(buffer[i]);
+   }*/
+}
+
+void loop(){
+}
+
+void EraseChip(){
+  FlashSSLow();
+  SPI.transfer(WRITE_ENABLE);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(ERASE_CHIP);
+  FlashSSHigh();
+
+  FlashSSLow();
+  SPI.transfer(READ_STATUS_REG);
+  statusByte = SPI.transfer(0);
+  Serial.println(statusByte,HEX);
+  FlashSSHigh();
+
+  Serial.println(millis());
+  while( (statusByte & 0x01) == 0x01){
+    FlashSSLow();
+    SPI.transfer(READ_STATUS_REG);
+    statusByte = SPI.transfer(0);
+    Serial.println(statusByte,HEX);
+    FlashSSHigh();
+    delay(5);
+  }
+  Serial.println(millis());  
+}
+
+
+
+
+
+
+
+
+
+
